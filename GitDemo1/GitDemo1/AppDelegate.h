06ad0710ad95b79123130de14599e9fbbb5ef4af
baixue <1228743613@qq.com>
2016-03-18 22:08:31 +0800

    first commit

---
+++ GitDemo1/GitDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo1
+//
+//  Created by baixue on 16/3/18.
+//  Copyright © 2016年 baixue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo1/GitDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo1
+//
+//  Created by baixue on 16/3/18.
+//  Copyright © 2016年 baixue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
